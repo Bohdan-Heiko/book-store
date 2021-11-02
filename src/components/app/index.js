22e067fdf4e74716db8d38263adff8a762953caf
Bohdan <bgejko@gmaol.com>
2021-11-02 17:30:47 +0200

    second commit

---
+++ src/components/app/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import s from './style.module.css'
+
+
+const App = () => {
+
+  return (
+    <div className={s.app}>
+      App
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/spiner/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import s from './style.module.css'
+
+
+const Spiner = () => {
+
+  return (
+    <div>Error!</div>
+  )
+}
+
+export default Spiner;
